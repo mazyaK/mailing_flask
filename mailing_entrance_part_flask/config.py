afc7146a89c2ad24d866767940d874fd85db1c55
sanoto <smazyak@bk.ru>
2020-11-24 22:41:26 +0300

    Update config.py

--- mailing_entrance_part_flask/config.py
+++ mailing_entrance_part_flask/config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:mazyakidze652@localhost:5432/mailing"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost:5432/mailing"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SECRET_KEY = '7a8fdb79ab334bad8bb17e9a4ccf9040'
 
